bf33a2ea09e1fe2cd1a85cb4f624ad2f2d40af4b
Stefan Niskanen Skoglund <stefan.skoglund@agj.net>
2022-04-13 18:12:08 +0200

    mer omfattande kommentarer i huvudet enligt spec från lab

--- mp5.c
+++ mp5.c
@@ -1,10 +1,13 @@
 /*
- * Time-stamp: <2022-04-01 22:05:08 stefan>
+ * Time-stamp: <2022-04-01 23:07:12 stefan>
  *
- * Stefan Niskanen Skoglund
- * ens21snd
+ * Mästarprov 5
  *
- * mp5.c (mästarprov 5)
+ * Fil:       mp5.c (mästarprov 5)
+ * Ansvarig:  Stefan Niskanen Skoglund
+ * Användare: ens21snd
+ *
+ * Se mp5.pdf
  *
  * https://github.com/skaraborgfakir/Umea_Universitet_C_programmering
  */
@@ -16,105 +19,12 @@
 #define VARUPRISER 2
 #define AVSLUTA 3
 
-/*
- * mp5.pdf: Krav på implementationen: 1.1 ska implementeras som en separat funktion
- *
- * skriver ut tillgängliga funktionsval i programmet
- * och inställd/vald valutakurs
- */
-void visa_meny(float valutakurs)
-{
-     printf( "\n");
-     printf( "1. Set exchange rate in SEK (current rate: %.2f)\n", valutakurs);
-     printf( "2. Convert prices from the foreign currency\n");
-     printf( "3. End\n");
-}
-
-/*
- * mp5.pdf: Krav på implementationen: 1.2 ska implementeras som en separat funktion
- */
-int funktionsval()
-{
-     int val = 0;
-
-     printf( "\nGive your choice (1 - %d): ", ANTALVAL);
-     scanf( "%d", &val);
-
-     return val;
-}
-
-/*
- * mp5.pdf: Krav på implementationen: 1.3.1 ska implementeras som en separat funktion
- */
-float ny_valutakurs()
-{
-     float ny_kurs;
-
-     printf( "\nGive exchange rate: ");
-     scanf("%f", &ny_kurs);
-
-     return ny_kurs;
-}
-
-/*
- * mp5.pdf: Krav på implementationen: 1.4.1.1 ska implementeras som en separat funktion
- *          Krav på implementationen: ska anropas från mata_in_priser (1.4.1 i flödet)
- */
-float mata_in_nytt_pris()
-{
-     float pris  = 0.0;
-
-     printf( "Give price (finish with < 0): ");
-     scanf( "%f", &pris);
-
-     return pris;
-}
-
-/*
- * mp5.pdf: Krav på implementationen: 1.4.1 ska implementeras som en separat funktion
- *
- * Funktion i 1.4.1.1 ska anropas av denna
- */
-float mata_in_priser()
-{
-     float summa = 0.0;
-     float pris  = 0.0;
-     int   klart = 0;
-
-     printf( "\n");
-
-     /*
-      * avsluta efter att ett negativt tal matas in
-      */
-     do
-     {
-	  pris = mata_in_nytt_pris();
-
-	  if (pris < 0)
-	  {
-	       klart = 1;
-	  }
-	  else
-	  {
-	       summa += pris;
-	  }
-     }
-     while (!klart);
-
-     return summa;
-}
-
-/*
- * mp5.pdf: Krav på implementationen: 1.4.2 ska implementeras som en separat funktion
- */
-void visa_summa_och_konvertera( float summa,     /* i lokal valuta */
-				float valutakurs /* omräkning till SEK */ )
-{
-     printf( "\nSum in foreign currency: %.2f\nSum in SEK: %.2f\n",
-	     summa,
-	     summa * valutakurs);
-}
-
+void visa_meny(float valutakurs);
+int funktionsval();
+float ny_valutakurs();
+float mata_in_priser();
+void visa_summa_och_konvertera( float summa, float valutakurs);
+float mata_in_nytt_pris();
 
 int main(int argc,char *argv[])
 {
@@ -160,8 +70,131 @@ int main(int argc,char *argv[])
 }
 
 /*
+ * Funktion: visa_menu
+ *
+ * mp5.pdf: Krav på implementationen: 1.1 ska implementeras som en separat funktion
+ *
+ * skriver ut tillgängliga funktionsval i programmet
+ * och inställd/vald valutakurs
+ *
+ * Indata:    vald valutakurs
+ * Utmatning: -
+ */
+void visa_meny(float valutakurs)
+{
+     printf( "\n");
+     printf( "1. Set exchange rate in SEK (current rate: %.2f)\n", valutakurs);
+     printf( "2. Convert prices from the foreign currency\n");
+     printf( "3. End\n");
+}
+
+/*
+ * mp5.pdf: Krav på implementationen: 1.2 ska implementeras som en separat funktion
+ *
+ * Indata:    -
+ * Utmatning: vilken funktion i programmet som ska användas
+ */
+int funktionsval()
+{
+     int val = 0;
+
+     printf( "\nGive your choice (1 - %d): ", ANTALVAL);
+     scanf( "%d", &val);
+
+     return val;
+}
+
+/*
+ * mp5.pdf: Krav på implementationen: 1.3.1 ska implementeras som en separat funktion
+ *
+ * Indata:    -
+ * Utmatning: vilken valutakurs som ska användas
+ */
+float ny_valutakurs()
+{
+     float ny_kurs;
+
+     printf( "\nGive exchange rate: ");
+     scanf("%f", &ny_kurs);
+
+     return ny_kurs;
+}
+
+
+/*
+ * mp5.pdf: Krav på implementationen: 1.4.1 ska implementeras som en separat funktion
+ *
+ * Funktion i 1.4.1.1 ska anropas av denna
+ *
+ * Indata:    -
+ * Utmatning: summa av alla varornas pris (i lokal valuta)
+ */
+float mata_in_priser()
+{
+     float summa = 0.0;
+     float pris  = 0.0;
+     int   klart = 0;
+
+     printf( "\n");
+
+     /*
+      * avsluta efter att ett negativt tal matas in
+      */
+     do
+     {
+	  pris = mata_in_nytt_pris();
+
+	  if (pris < 0)
+	  {
+	       klart = 1;
+	  }
+	  else
+	  {
+	       summa += pris;
+	  }
+     }
+     while (!klart);
+
+     return summa;
+}
+
+/*
+ * mp5.pdf: Krav på implementationen: 1.4.1.1 ska implementeras som en separat funktion
+ *          Krav på implementationen: ska anropas från mata_in_priser (1.4.1 i flödet)
+ *
+ * Indata:    -
+ * Utmatning: någon saks pris (i lokal valuta)
+ */
+float mata_in_nytt_pris()
+{
+     float pris  = 0.0;
+
+     printf( "Give price (finish with < 0): ");
+     scanf( "%f", &pris);
+
+     return pris;
+}
+
+/*
+ * mp5.pdf: Krav på implementationen: 1.4.2 ska implementeras som en separat funktion
+ *
+ * Indata:    vald valutakurs
+ *            summa i lokal valuta
+ * Utmatning: -
+ */
+void visa_summa_och_konvertera( float summa,     /* i lokal valuta */
+				float valutakurs /* omräkning till SEK */ )
+{
+     printf( "\nSum in foreign currency: %.2f\nSum in SEK: %.2f\n",
+	     summa,
+	     summa * valutakurs);
+}
+
+
+
+/*
  * Local Variables:
  * c-file-style: "k&r"
- * compile-command: "/usr/bin/gcc -Wall --std=c99 mp5.c -o mp5"
+ * compile-command: "make"
  * End:
  */
