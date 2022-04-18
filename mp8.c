3f7d48355e8b38baa8c624639672a6b9c4ae72db
Stefan Niskanen Skoglund <stefan.skoglund@agj.net>
2022-04-18 14:24:23 +0200

    formattering av koden (från ellemtel till k&r)

--- mp8.c
+++ mp8.c
@@ -1,6 +1,6 @@
 /*
  * Programmering i C
- * Time-stamp: <2022-04-18 12:53:11 stefan>
+ * Time-stamp: <2022-04-18 14:17:58 stefan>
  * Spring 22
  * Mastery test 8
  *
@@ -27,8 +27,8 @@
 
 /* Declaration of data structure */
 typedef struct {
-   char current;
-   char next;
+     char current;
+     char next;
 } cell;
 /* exemplet i peppar använder
  *
@@ -55,117 +55,117 @@ int antalgrannar   ( const int nrows, const int ncols, cell atlas[nrows][ncols],
 
 int main( void)
 {
-   /*
-    * krav : fixa en värld (matris av celler) med en visst antal positioner
-    * exemplet i peppar använder
-    *
-    * exemplet använder 20x20 så låt oss göra så
-    */
-   int avsluta = 0;
-   cell cellerna[nrows][ncols];
-   const int bufferdim = 10;
-   char buffer[bufferdim];
+     /*
+      * krav : fixa en värld (matris av celler) med en visst antal positioner
+      * exemplet i peppar använder
+      *
+      * exemplet använder 20x20 så låt oss göra så
+      */
+     int avsluta = 0;
+     cell cellerna[nrows][ncols];
+     const int bufferdim = 10;
+     char buffer[bufferdim];
 
-   init_field( nrows, ncols, cellerna);
+     init_field( nrows, ncols, cellerna);
 
-   while(!avsluta)
-   {
-      cellutskrift( nrows, ncols, cellerna);
+     while(!avsluta)
+     {
+	  cellutskrift( nrows, ncols, cellerna);
 
-      /* iterering igenom cellerna för att hitta nödvändiga förändringar
-       *
-       * se: https://conwaylife.com/wiki/Conway%27s_Game_of_Life
-       */
-      for ( int rad = 0 ; rad < nrows ; rad++)
-      {
-	 for ( int kolumn = 0 ; kolumn < ncols ; kolumn++)
-	 {
-	    int grannar = antalgrannar( nrows, ncols, cellerna, rad, kolumn);
+	  /* iterering igenom cellerna för att hitta nödvändiga förändringar
+	   *
+	   * se: https://conwaylife.com/wiki/Conway%27s_Game_of_Life
+	   */
+	  for ( int rad = 0 ; rad < nrows ; rad++)
+	  {
+	       for ( int kolumn = 0 ; kolumn < ncols ; kolumn++)
+	       {
+		    int grannar = antalgrannar( nrows, ncols, cellerna, rad, kolumn);
 
-	    switch ( cellerna[rad][kolumn].current )
-	    {
-	       case ALIVE:
-		  if ( grannar < 2 )      /* inte tillräckligt mycket stöd från grannar för att överleva */
-		     cellerna[rad][kolumn].next = DEAD;
-		  else if ( grannar > 3 ) /* överbefolkning */
-		     cellerna[rad][kolumn].next = DEAD;
-		  else                    /* implicit två eller tre levande grannar - överlever */
-		     cellerna[rad][kolumn].next = ALIVE;
-		  break;
+		    switch ( cellerna[rad][kolumn].current )
+		    {
+		    case ALIVE:
+			 if ( grannar < 2 )      /* inte tillräckligt mycket stöd från grannar för att överleva */
+			      cellerna[rad][kolumn].next = DEAD;
+			 else if ( grannar > 3 ) /* överbefolkning */
+			      cellerna[rad][kolumn].next = DEAD;
+			 else                    /* implicit två eller tre levande grannar - överlever */
+			      cellerna[rad][kolumn].next = ALIVE;
+			 break;
 
-	       case DEAD:
-	       default:
-		  if ( grannar == 3) /* tre levande grannar så en klon migrerar till cellen */
-		     cellerna[rad][kolumn].next = ALIVE;
-		  else
-		     cellerna[rad][kolumn].next = DEAD;
-	    }
-	 }
-      }
+		    case DEAD:
+		    default:
+			 if ( grannar == 3) /* tre levande grannar så en klon migrerar till cellen */
+			      cellerna[rad][kolumn].next = ALIVE;
+			 else
+			      cellerna[rad][kolumn].next = DEAD;
+		    }
+	       }
+	  }
 
-      /*
-       * låt en generation gå
-       */
-      for ( int rad = 0 ; rad < nrows ; rad++)
-      {
-	 for ( int kolumn = 0 ; kolumn < ncols ; kolumn++)
-	 {
-	    switch ( cellerna[rad][kolumn].current )
-	    {
-	       case ALIVE:
-		  if ( cellerna[rad][kolumn].next == ALIVE )
-		     cellerna[rad][kolumn].current = ALIVE;
-		  else
-		     cellerna[rad][kolumn].current = DEAD;
-		  break;
+	  /*
+	   * låt en generation gå
+	   */
+	  for ( int rad = 0 ; rad < nrows ; rad++)
+	  {
+	       for ( int kolumn = 0 ; kolumn < ncols ; kolumn++)
+	       {
+		    switch ( cellerna[rad][kolumn].current )
+		    {
+		    case ALIVE:
+			 if ( cellerna[rad][kolumn].next == ALIVE )
+			      cellerna[rad][kolumn].current = ALIVE;
+			 else
+			      cellerna[rad][kolumn].current = DEAD;
+			 break;
 
-	       case DEAD:
-		  if ( cellerna[rad][kolumn].next == ALIVE )
-		     cellerna[rad][kolumn].current = ALIVE;
-	    }
-	 }
-      }
+		    case DEAD:
+			 if ( cellerna[rad][kolumn].next == ALIVE )
+			      cellerna[rad][kolumn].current = ALIVE;
+		    }
+	       }
+	  }
 
-      printf( "Select one of the following options:\n(enter) Step\n(any)   Exit\n");
+	  printf( "Select one of the following options:\n(enter) Step\n(any)   Exit\n");
 
-      char *input = fgets( buffer, bufferdim-1, stdin);
+	  char *input = fgets( buffer, bufferdim-1, stdin);
 
-      if ( input == 0)
-	 avsluta = 1; /* 0 från fgets, inget att läsa - tryckte användaren på ^D ? */
-      else
-      {
-	 if ( strlen( buffer) == 1 && !strcmp( "\n", buffer)) /* tom rad */
-	    avsluta = 0;
-	 else
-	    avsluta = 1;
-      }
-   }
+	  if ( input == 0)
+	       avsluta = 1; /* 0 från fgets, inget att läsa - tryckte användaren på ^D ? */
+	  else
+	  {
+	       if ( strlen( buffer) == 1 && !strcmp( "\n", buffer)) /* tom rad */
+		    avsluta = 0;
+	       else
+		    avsluta = 1;
+	  }
+     }
 
-   return 0;
+     return 0;
 }
 
 int antalgrannar( const int antal_rader, const int antal_kolumner, cell cellerna[antal_rader][antal_kolumner],
 		  const int cellens_rad, const int cellens_kolumn)
 {
-   /*
-    * algoritm:
-    *   påbörja sökning i cell snett uppåt vänster från aktuell cell
-    *   därefter sök av radvis, börja i vänster
-    *   rad två, den mittersta är aktuell cell
-    */
-   int antalgrannar = 0;
+     /*
+      * algoritm:
+      *   påbörja sökning i cell snett uppåt vänster från aktuell cell
+      *   därefter sök av radvis, börja i vänster
+      *   rad två, den mittersta är aktuell cell
+      */
+     int antalgrannar = 0;
 
-   for ( int rad = cellens_rad-1 ; rad <= cellens_rad + 1 ; rad++) /* sök igenom 3 rader */
-   {
-      for ( int kolumn = cellens_kolumn-1 ; kolumn <= cellens_kolumn + 1 ; kolumn++) /* sök igenom 3 kolumner */
-	 if ( rad >= 0 && kolumn >= 0                            &&  /* kontroll att sökningen är i cellerna */
-	      rad < antal_rader && kolumn < antal_kolumner       &&  /* inte nedanför eller för långt till höger */
-	      !( rad == cellens_rad && kolumn == cellens_kolumn) &&  /* se till att inte få med cellen självt !!! */
-	      cellerna[rad][kolumn].current == ALIVE)                /* lever grannen ? */
-	    antalgrannar ++;
-   }
+     for ( int rad = cellens_rad-1 ; rad <= cellens_rad + 1 ; rad++) /* sök igenom 3 rader */
+     {
+	  for ( int kolumn = cellens_kolumn-1 ; kolumn <= cellens_kolumn + 1 ; kolumn++) /* sök igenom 3 kolumner */
+	       if ( rad >= 0 && kolumn >= 0                            &&  /* kontroll att sökningen är i cellerna */
+		    rad < antal_rader && kolumn < antal_kolumner       &&  /* inte nedanför eller för långt till höger */
+		    !( rad == cellens_rad && kolumn == cellens_kolumn) &&  /* se till att inte få med cellen självt !!! */
+		    cellerna[rad][kolumn].current == ALIVE)                /* lever grannen ? */
+		    antalgrannar ++;
+     }
 
-   return antalgrannar;
+     return antalgrannar;
 }
 
 
@@ -173,13 +173,13 @@ int antalgrannar( const int antal_rader, const int antal_kolumner, cell cellerna
  */
 void cellutskrift( const int antal_rader, const int antal_kolumner, cell cellerna[antal_rader][antal_kolumner] )
 {
-   for (int rad = 0 ; rad < antal_rader ; rad++) {
-      for (int kolumn = 0 ; kolumn < antal_kolumner ; kolumn++) {
-	 putc( cellerna[rad][kolumn].current, stdout);
-	 printf( " ");
-      }
-      putc( '\n', stdout);
-   }
+     for (int rad = 0 ; rad < antal_rader ; rad++) {
+	  for (int kolumn = 0 ; kolumn < antal_kolumner ; kolumn++) {
+	       putc( cellerna[rad][kolumn].current, stdout);
+	       printf( " ");
+	  }
+	  putc( '\n', stdout);
+     }
 }
 
 /* Description: Initialize all the cells to dead, then asks the user
@@ -194,41 +194,41 @@ void cellutskrift( const int antal_rader, const int antal_kolumner, cell cellern
 void init_field( const int rows, const int cols,
 		 cell field[rows][cols])
 {
-   for (int r = 0 ; r < rows ; r++) {
-      for (int c = 0 ; c < cols ; c++) {
-	 field[r][c].current = DEAD;
-      }
-   }
+     for (int r = 0 ; r < rows ; r++) {
+	  for (int c = 0 ; c < cols ; c++) {
+	       field[r][c].current = DEAD;
+	  }
+     }
 
-   printf("Select field spec to load ");
-   printf("([G]lider, [S]emaphore, ");
-   printf("[R]andom or [C]ustom): ");
+     printf("Select field spec to load ");
+     printf("([G]lider, [S]emaphore, ");
+     printf("[R]andom or [C]ustom): ");
 
-   int ch = getchar();
+     int ch = getchar();
 
-   /* Ignore following newline */
-   if (ch != '\n') {
-      getchar();
-   }
+     /* Ignore following newline */
+     if (ch != '\n') {
+	  getchar();
+     }
 
-   switch (ch) {
-      case 'g':
-      case 'G':
-	 load_glider(rows, cols, field);
-	 break;
-      case 's':
-      case 'S':
-	 load_semaphore(rows, cols, field);
-	 break;
-      case 'r':
-      case 'R':
-	 load_random(rows, cols, field);
-	 break;
-      case 'c':
-      case 'C':
-      default:
-	 load_custom(rows, cols, field);
-   }
+     switch (ch) {
+     case 'g':
+     case 'G':
+	  load_glider(rows, cols, field);
+	  break;
+     case 's':
+     case 'S':
+	  load_semaphore(rows, cols, field);
+	  break;
+     case 'r':
+     case 'R':
+	  load_random(rows, cols, field);
+	  break;
+     case 'c':
+     case 'C':
+     default:
+	  load_custom(rows, cols, field);
+     }
 }
 
 
@@ -242,11 +242,11 @@ void init_field( const int rows, const int cols,
 void load_glider(const int rows, const int cols,
 		 cell field[rows][cols])
 {
-   field[0][1].current = ALIVE;
-   field[1][2].current = ALIVE;
-   field[2][0].current = ALIVE;
-   field[2][1].current = ALIVE;
-   field[2][2].current = ALIVE;
+     field[0][1].current = ALIVE;
+     field[1][2].current = ALIVE;
+     field[2][0].current = ALIVE;
+     field[2][1].current = ALIVE;
+     field[2][2].current = ALIVE;
 }
 
 
@@ -260,9 +260,9 @@ void load_glider(const int rows, const int cols,
 void load_semaphore(const int rows, const int cols,
 		    cell field[rows][cols])
 {
-   field[8][1].current = ALIVE;
-   field[8][2].current = ALIVE;
-   field[8][3].current = ALIVE;
+     field[8][1].current = ALIVE;
+     field[8][2].current = ALIVE;
+     field[8][3].current = ALIVE;
 }
 
 
@@ -277,24 +277,24 @@ void load_semaphore(const int rows, const int cols,
 void load_random(const int rows, const int cols,
 		 cell field[rows][cols])
 {
-   /*
-    * slumptalet behöver ha en seed - klassisk, använd klockan
-    */
+     /*
+      * slumptalet behöver ha en seed - klassisk, använd klockan
+      */
 
-   for (int x = 0; x < cols; x++)
-   {
-      for (int y = 0; y < rows; y++)
-      {
-	 double slump = drand48();
-	 /*
-	  * högre än 0,5 lev !
-	  */
-	 if ( slump > 0.5 )
-	    field[y][x].current = ALIVE;
-	 else
-	    field[y][x].current = DEAD;
-      }
-   }
+     for (int x = 0; x < cols; x++)
+     {
+	  for (int y = 0; y < rows; y++)
+	  {
+	       double slump = drand48();
+	       /*
+		* högre än 0,5 lev !
+		*/
+	       if ( slump > 0.5 )
+		    field[y][x].current = ALIVE;
+	       else
+		    field[y][x].current = DEAD;
+	  }
+     }
 }
 
 
@@ -309,19 +309,19 @@ void load_random(const int rows, const int cols,
 void load_custom(const int rows, const int cols,
 		 cell field[rows][cols])
 {
-   printf( "Give custom format string: ");
-   do
-   {
-      int r, c;
-      scanf( "%d,%d", &r, &c);
-      field[r][c].current = ALIVE;
-   }
-   while ( getchar() != '\n');
+     printf( "Give custom format string: ");
+     do
+     {
+	  int r, c;
+	  scanf( "%d,%d", &r, &c);
+	  field[r][c].current = ALIVE;
+     }
+     while ( getchar() != '\n');
 }
 
 /*
  * Local Variables:
- * c-file-style: "ellemtel"
+ * c-file-style: "k&r"
  * compile-command: "make"
  * End:
  */
